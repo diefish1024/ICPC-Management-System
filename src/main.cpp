f85118628bddd809d20687d085c2dce98269782f
huangjiehang <huangjiehang@gmail.com>
2025-02-16 14:36:29 +0800

    upd: options before START

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,6 @@ int main() {
     InputBuffer input_buffer;
 
     int duration_time = 0;
-    int problem_count = 0;
 
     // bufore START
     while (true) {
@@ -21,33 +20,43 @@ int main() {
         std::string token;
         iss >> token;
         if (token == "ADDTEAM") {
+        // if (token[0] == 'A') {
             // ADDTEAM [team_name]
             std::string name;
             iss >> name;
             if (!team_manager.add_team(name)) {
                 std::cout << "[Error]Add failed: duplicated team name.\n";
             } else {
+                // DEBUG
+                // team_manager.last_team().print_team();
+
                 rank_tree.insert(new RBNode(&team_manager.last_team()));
                 std::cout << "[Info]Add successfully.\n";
             }
         }
         if (token == "START") {
+        // if (token[0] == 'S') {
             // START DURATION [duration_time] PROBLEM [problem_count]
-            iss >> token >> duration_time >> token >> problem_count;
+            iss >> token >> duration_time >> token >> Team::problem_count;
 
             // DEBUG
-            std::cout << "duration_time: " << duration_time << std::endl;
-            std::cout << "problem_count: " << problem_count << std::endl;
+            // std::cout << "duration_time: " << duration_time << std::endl;
+            // std::cout << "problem_count: " << Team::problem_count << std::endl;
+
+            std::cout << "[Info]Competition starts.\n";
             break;
         }
 
         // DEBUFG
-        if (token == "p") {
+        if (token == "p1") {
             rank_tree.print_tree();
+        }
+        if (token == "p2") {
+            team_manager.print_all_teams();
         }
     }
 
-    rank_tree.print_tree();
+    // rank_tree.print_tree();
 
     return 0;
 }
